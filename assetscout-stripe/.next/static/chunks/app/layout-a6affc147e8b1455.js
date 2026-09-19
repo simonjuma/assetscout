eb4e3a970071b9bc05c27e0666fdcf1c49f2423a
Simon Juma <simon452juma@gmail.com>
2026-09-19 23:42:16 +0300

    untrack .next folder and confirm gitignore rules

--- assetscout-stripe/.next/static/chunks/app/layout-a6affc147e8b1455.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3673:()=>{},8743:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,3673,23))}},e=>{e.O(0,[978,619,441,255,358],()=>e(e.s=8743)),_N_E=e.O()}]);
